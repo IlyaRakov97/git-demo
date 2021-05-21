8c98b405c46562c60517e1c0ab44303ccbc6abab
IlyaRakov97 <84500483+IlyaRakov97@users.noreply.github.com>
2021-05-21 18:15:58 +0400

    add anymore lines for song

--- song.php
+++ song.php
@@ -10,7 +10,20 @@ I was told when I get older
 All my fears would shrink
 But now I'm insecure
 And I care what people think
-
+Wish we could turn back time
+To the good old days
+When our momma sang us to sleep
+But now we're stressed out
+Wish we could turn back time
+To the good old days
+When our momma sang us to sleep
+But now we're stressed out
+We're stressed out
+Sometimes a certain smell will
+Take me back to when I was young
+How come I'm never able to identify
+Where it's coming from?
+I'd make…
 
 
 
